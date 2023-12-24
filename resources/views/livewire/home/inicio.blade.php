8ec83dd9d7de3b2fa2e64a39dd913fc0343b55a5
Cruz Wilfrido Ek Martin <gamecruz@gmail.com>
2023-12-23 22:44:37 -0500

    23 componente para las tablas

--- resources/views/livewire/home/inicio.blade.php
+++ resources/views/livewire/home/inicio.blade.php
@@ -6,7 +6,22 @@
         <x-slot:cardTools>
             <a href="#" class="btn btn-primary">Crear</a>
         </x-slot:cardTools>
-        Contenido
+
+        <x-table>
+
+            <x-slot:thead>
+                <th>thead</th>
+
+                <th>thead</th>
+            </x-slot:thead>
+
+
+                <tr>
+                    <td>...</td>
+                    <td>...</td>
+                </tr>
+
+        </x-table>
 
     </x-card>
 
